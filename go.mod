68e723ac3ff2b535ec3eb6ca40315e742e2990a8
zhangli2946 <zhangli2946@gmail.com>
2025-04-21 00:57:17 +0800

    bring up repository generic

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/go4s/repository
+
+go 1.18
+
+require (
+	github.com/go4s/util v1.2.6
+	xorm.io/xorm v1.3.9
+)
+
+require (
+	github.com/goccy/go-json v0.8.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
+)
---
+++ repository.go
@@ -0,0 +1,173 @@
+package repository
+
+import (
+    "context"
+    
+    "github.com/go4s/util/convertor"
+    "xorm.io/xorm"
+)
+
+type (
+    Option interface {
+        Decorate(*xorm.Session) (*xorm.Session, error)
+    }
+    OptionModifier[T Option] func(*T)
+)
+
+type (
+    DeleteBehavior[T any, O Option, M OptionModifier[O]] interface {
+        Delete(context.Context, ...M) (int64, error)
+    }
+)
+
+type (
+    UpdateBehavior[T any, O Option, M OptionModifier[O]] interface {
+        Update(context.Context, convertor.Into[T], ...M) error
+    }
+    CreateBehavior[T any, O Option, M OptionModifier[O]] interface {
+        Create(context.Context, convertor.Into[T], ...M) error
+    }
+    UpsertBehavior[T any, O Option, M OptionModifier[O]] interface {
+        Upsert(context.Context, convertor.Into[T], ...M) error
+    }
+)
+
+type (
+    QueryBehavior[T any, O Option, M OptionModifier[O]] interface {
+        Query(context.Context, convertor.From[T], ...M) error
+    }
+)
+
+type (
+    MigrationBehavior[T any, O Option, M OptionModifier[O]] interface {
+        InjectEngine(context.Context, *xorm.Engine, ...M) error
+    }
+)
+type (
+    Repository[T any, O Option, M OptionModifier[O]] struct {
+        engine *xorm.Engine
+    }
+)
+
+func (r *Repository[T, O, M]) InjectEngine(ctx context.Context, engine *xorm.Engine, modifiers ...M) (err error) {
+    var (
+        schema = new(T)
+        o      = new(O)
+        sess   *xorm.Session
+    )
+    for _, modifier := range modifiers {
+        modifier(o)
+    }
+    if r.engine = engine; o == nil {
+        panic("option nil")
+    }
+    if sess, err = (*o).Decorate(r.engine.Context(ctx)); err != nil {
+        return
+    }
+    if err = sess.Sync2(schema); err != nil {
+        return
+    }
+    return sess.Close()
+}
+func (r *Repository[T, O, M]) setupSession(ctx context.Context, modifiers ...M) (sess *xorm.Session, err error) {
+    var o O
+    for _, modifier := range modifiers {
+        modifier(&o)
+    }
+    return o.Decorate(r.engine.Context(ctx))
+}
+func (r *Repository[T, O, M]) Query(ctx context.Context, gather convertor.From[T], modifiers ...M) (err error) {
+    var (
+        sess *xorm.Session
+    )
+    if sess, err = r.setupSession(ctx, modifiers...); err != nil {
+        return
+    }
+    var ts = []T{}
+    if err = sess.Find(&ts); err != nil {
+        return
+    }
+    for _, t := range ts {
+        if err = gather.From(t); err != nil {
+            return
+        }
+    }
+    return
+}
+func (r *Repository[T, O, M]) Update(ctx context.Context, value convertor.Into[T], modifiers ...M) (err error) {
+    var (
+        val  T
+        sess *xorm.Session
+    )
+    if sess, err = r.setupSession(ctx, modifiers...); err != nil {
+        return
+    }
+    
+    if val, err = value.Into(); err != nil {
+        return
+    }
+    if _, err = sess.Update(val); err != nil {
+        return
+    }
+    return
+}
+func (r *Repository[T, O, M]) Create(ctx context.Context, value convertor.Into[[]interface{}], modifiers ...M) (err error) {
+    
+    var (
+        vals []interface{}
+        sess *xorm.Session
+    )
+    if sess, err = r.setupSession(ctx, modifiers...); err != nil {
+        return
+    }
+    
+    if vals, err = value.Into(); err != nil || len(vals) == 0 {
+        return
+    }
+    if _, err = sess.Insert(vals...); err != nil {
+        return
+    }
+    return
+}
+func (r *Repository[T, O, M]) Upsert(ctx context.Context, value convertor.Into[T], modifiers ...M) (err error) {
+    
+    var (
+        val  T
+        cnt  int64
+        sess *xorm.Session
+    )
+    if sess, err = r.setupSession(ctx, modifiers...); err != nil {
+        return
+    }
+    if cnt, err = sess.Count(&val); err != nil {
+        return
+    }
+    if cnt == 0 {
+        if _, err = sess.Insert(value); err != nil {
+            return
+        }
+        return
+    }
+    if val, err = value.Into(); err != nil {
+        return
+    }
+    if cnt, err = sess.Update(val); err != nil {
+        return
+    }
+    return
+}
+func (r *Repository[T, O, M]) Delete(ctx context.Context, modifiers ...M) (cnt int64, err error) {
+    var (
+        bean = new(T)
+        sess *xorm.Session
+    )
+    if sess, err = r.setupSession(ctx, modifiers...); err != nil {
+        return
+    }
+    cnt, err = sess.Delete(bean)
+    return
+}
+
+func New[T any, O Option]() Repository[T, O, OptionModifier[O]] {
+    return Repository[T, O, OptionModifier[O]]{}
+}
